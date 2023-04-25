5be063c75f62df95039d593ac0646b03deb58cab
Programster <sdpagent@gmail.com>
2023-04-25 12:46:31 +0100

    updates the file logger to output in a human readable date format, but takes an optional string to define the format of the output.

--- src/FileLogger.php
+++ src/FileLogger.php
@@ -14,12 +14,18 @@ final class FileLogger extends AbstractLogger
 {
     protected $m_fileHandle;
 
+    protected string $m_dateFormat;
+
 
     /**
-     * Creates a logger that logs to a file.
+     * Create a logger that logs to a CSV file.
+     * @param string $filepath - the path to the file you wish to log to.
+     * @param string $timeFormat - the format you want the time in. Defaults to Y-m-d H:i:s for easy sorting and reading.
+     * @throws ExceptionFailedToOpenFile - if fails to open the file.
      */
-    public function __construct(string $filepath)
+    public function __construct(string $filepath, string $timeFormat="Y-m-d H:i:s")
     {
+        $this->m_dateFormat = $timeFormat;
         $this->m_fileHandle = fopen($filepath, "a");
 
         if ($this->m_fileHandle === false)
@@ -30,13 +36,11 @@ final class FileLogger extends AbstractLogger
 
 
     /**
-     * Logs with an arbitrary level.
-     *
-     * @param string $level - the level of the logl
-     * @param string $message -  the message of the error, e.g "failed to connect to db"
-     * @param array $context - name value pairs providing context to error, e.g. "dbname => "yolo")
-     *
+     * @param $level - the level of the log, may be a string (e.g. "emergency") or integer (0-7)
+     * @param string|\Stringable $message - the message of the error, e.g "failed to connect to db"
+     * @param array $context - - name value pairs providing context to error, e.g. "dbname => "yolo")
      * @return void
+     * @throws Exceptions\ExceptionInvalidLogLevel - if provided an invalid log level
      */
     public function log($level, string|\Stringable $message, array $context = array()) : void
     {
@@ -44,7 +48,7 @@ final class FileLogger extends AbstractLogger
         $jsonContextString = json_encode($context, JSON_UNESCAPED_SLASHES);
 
         $data = array(
-            microtime(true),
+            date($this->m_dateFormat, time()),
             $level,
             $message,
             $jsonContextString
--- testing/TestFileLogger.php
+++ testing/TestFileLogger.php
@@ -15,11 +15,17 @@ final class TestFileLogger
         $logFilepath = tempnam(sys_get_temp_dir(), "logs-");
         $logger = new \Programster\Log\FileLogger($logFilepath);
         $logger->debug("This is an info log", ['name' => 'value']);
+        sleep(1);
         $logger->notice("This is a notice log", ['name' => 'value']);
+        sleep(1);
         $logger->info("This is an info log", ['name' => 'value']);
+        sleep(1);
         $logger->warning("This is a warning log", ['name' => 'value']);
+        sleep(1);
         $logger->alert("This is an alert log", ['name' => 'value']);
+        sleep(1);
         $logger->emergency("This is an emergency log", ['name' => 'value']);
+        sleep(1);
         $logger->critical("This is a critical log", ['name' => 'value']);
         $logger->log(Psr\Log\LogLevel::WARNING, "This is another warning log", ['name' => 'value']);
         $logger->info("You can log with just a message and no context.");
